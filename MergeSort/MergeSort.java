4450058e0f85ab4f3bc060bc0d463daceaf6c9be
eN1nja <mr.buntar@gmail.com>
2024-02-04 16:51:52 +0300

    first commit

---
+++ MergeSort/MergeSort.java
@@ -0,0 +1,61 @@
+public class MergeSort {
+    public static void merge(int[] A, int p, int q, int r) {
+        int n1 = q - p + 1;
+        int n2 = r - q;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            L[i] = A[p + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = A[q + 1 + j];
+        }
+
+        int i = 0;
+        int j = 0;
+        int k = p;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                A[k] = L[i];
+                i++;
+            } else {
+                A[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            A[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            A[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    public static void mergeSort(int[] A, int p, int r) {
+        if (p < r) {
+            int q = (p + r) / 2;
+            mergeSort(A, p, q);
+            mergeSort(A, q + 1, r);
+            merge(A, p, q, r);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] A = {5, 2, 4, 6, 1, 3, 2, 6};
+        mergeSort(A, 0, A.length - 1);
+        for (int num : A) {
+            System.out.print(num + " ");
+        }
+        // Output: 1 2 2 3 4 5 6 6
+    }
+}
